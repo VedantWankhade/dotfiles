7f270baa7ccbb2fbf4c4a9092001be7d5e9cc629
Vedant Wankhade <vedantwankhade17@gmail.com>
2025-07-27 18:33:11 +0530

    nvim tabstop and colorscheme

--- config/nvim/lua/custom/options.lua
+++ config/nvim/lua/custom/options.lua
@@ -6,7 +6,7 @@ vim.opt.termguicolors = true    -- 24 bit terminal colors
 -- tabs to spaces
 vim.opt.expandtab = true    -- convert tab to spaces
 vim.opt.softtabstop = 4     -- insert 4 spaces for a tab
-vim.opt.tabstop = 8         -- keep this at 8 so that editing operations like using 'x' to delete will work on single character (space), but hitting backspace deletes as tab
+vim.opt.tabstop = 4         -- keep this at 4 so that editing operations like using 'x' to delete will work on single character (space), but hitting backspace deletes as tab
 vim.opt.shiftwidth = 4      -- keep same as softtabstop
 
 vim.opt.scrolloff = 20      -- keep the cursor above 20 lines
