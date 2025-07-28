3b8e375d622b031fcfdb9868a4e39b164c48dc5c
Vedant Wankhade <vedantwankhade17@gmail.com>
2025-07-28 18:54:21 +0530

    nvim | import and format on save

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -1,36 +1,39 @@
-vim.lsp.enable('gopls')
+local lspconfig = require('lspconfig')
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method('textDocument/implementation') then
-      -- Create a keymap for vim.lsp.buf.implementation ...
-    end
-
-    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-    -- if client:supports_method('textDocument/completion') then
-    --   -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-    --   local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-    --   client.server_capabilities.completionProvider.triggerCharacters = chars
-    --   vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
-    -- end
-
-    
-    -- Auto-format ("lint") on save.
-    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-    if not client:supports_method('textDocument/willSaveWaitUntil')
-        and client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
+lspconfig.gopls.setup({
+  on_attach = function(client, bufnr)
+    -- Organize imports before saving
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+        for _, res in pairs(result or {}) do
+          for _, action in pairs(res.result or {}) do
+            if action.edit then
+              vim.lsp.util.apply_workspace_edit(action.edit, "utf-16")
+            else
+              vim.lsp.buf.execute_command(action.command)
+            end
+          end
+        end
+      end,
+    })
   end,
 })
+
+-- Format and organize imports
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = bufnr,
+  callback = function()
+    vim.lsp.buf.format({ async = false })  -- format code
+    -- organize imports (as above)
+  end,
+})
+
+
+vim.lsp.enable('gopls')
 
 vim.diagnostic.config({
   virtual_text = true,  -- shows inline error/warning text
